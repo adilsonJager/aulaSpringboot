df5f6ca7d1d6da788d0c4b3242fda9ff08d6c603
Adilson Magalhaes Jager <adilsonjager@gmail.com>
2026-01-08 15:11:58 +0000

    first controller

---
+++ src/main/java/com/example/aulaspringboot/Controller.java
@@ -0,0 +1,16 @@
+package com.example.aulaspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "WELCOME my web site";
+    }
+
+}
