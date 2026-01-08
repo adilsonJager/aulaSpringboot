3d8388d342ce4811ef167964ce976e6fbef7b88f
Adilson Magalhaes Jager <adilsonjager@gmail.com>
2026-01-08 19:16:12 +0000

    lombok

--- src/main/java/com/example/aulaspringboot/Ninjas/NinjaModel.java
+++ src/main/java/com/example/aulaspringboot/Ninjas/NinjaModel.java
@@ -2,11 +2,16 @@ package com.example.aulaspringboot.Ninjas;
 
 import com.example.aulaspringboot.Missoes.MissionModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
+
 @Entity
 @Table(name= "tb_register")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data // create gets and setters
 public class NinjaModel {
 
     @Id
@@ -22,35 +27,4 @@ public class NinjaModel {
     private MissionModel mission;
 
 
-    public NinjaModel() {}
-
-    public NinjaModel(String name, String email, int idade) {
-        this.name = name;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
