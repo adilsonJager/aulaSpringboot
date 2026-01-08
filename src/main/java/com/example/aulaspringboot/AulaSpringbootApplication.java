f5f62dd4623f3cc287618b6ea6fe14a00cbf3da4
Adilson Magalhaes Jager <adilsonjager@gmail.com>
2026-01-08 14:26:51 +0000

    Initial commit

---
+++ src/main/java/com/example/aulaspringboot/AulaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.aulaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AulaSpringbootApplication.class, args);
+    }
+
+}
